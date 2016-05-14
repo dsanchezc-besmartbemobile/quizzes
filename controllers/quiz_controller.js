8b0cd35eda34ce7b6ed114e37e4e3187be676412
NIce <dsanchezc@vectoritcgroup.com>
2016-05-14 11:14:48 +0200

    c

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,11 @@
 
 exports.question=function(req,res,nest){
-	res.render('quizzes/question', {question: 'Cual es la capital de Italia'});
+	var answer = req.query.answer || '';
+	res.render('quizzes/question', {question: 'Cual es la capital de Italia',answer: answer});
 };
 
 exports.check = function(req,res,next) {
+	var answer =  req.query.answer || '';
 	var resultado = ((req.query.answer == 'Roma' ) ? 'Correcta' : 'Incorrecta');
 	res.render('quizzes/result', {result : resultado });
 };
