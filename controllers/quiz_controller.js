022cffe91a733bb9b5d0e689bc849bac6aab20ae
NIce <dsanchezc@vectoritcgroup.com>
2016-05-14 12:33:06 +0200

    a

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,42 @@
 
-exports.question=function(req,res,nest){
-	var answer = req.query.answer || '';
-	res.render('quizzes/question', {question: 'Cual es la capital de Italia',answer: answer});
+var models = require('../models');
+
+// GET /question
+exports.question = function(req, res, next) {
+	models.Quiz.findOne() // Busca la primera pregunta existente
+		.then(function(quiz) {
+			if (quiz) {
+				var answer = req.query.answer || '';
+
+				res.render('quizzes/question', {question: quiz.question,
+								                answer: answer});
+		    } else {
+		    	throw new Error('No hay preguntas en la BBDD.');
+		    }
+		})
+		.catch(function(error) {
+			next(error);
+		});
 };
 
-exports.check = function(req,res,next) {
-	var answer =  req.query.answer || '';
-	var resultado = ((req.query.answer == 'Roma' ) ? 'Correcta' : 'Incorrecta');
-	res.render('quizzes/result', {result : resultado, answer:answer });
+
+// GET /check
+exports.check = function(req, res, next) {
+	models.Quiz.findOne() // Busca la primera pregunta existente
+		.then(function(quiz) {
+			if (quiz) {
+				var answer = req.query.answer || "";
+
+				var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+
+				res.render('quizzes/result', { result: result, 
+											   answer: answer });
+		    } else {
+		    	throw new Error('No hay preguntas en la BBDD.');
+		    }
+		})
+		.catch(function(error) {
+			next(error);
+		});
 };
+
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log(5 + 6);
   res.render('index');
 });
 
 router.get('/question', quizController.question);
-router.get('/check', quizController.check);
-
+router.get('/check',    quizController.check);
 
 module.exports = router;
