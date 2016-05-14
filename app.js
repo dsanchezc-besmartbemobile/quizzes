aac4087b5896aacf2c76e6efdcd282ae49858bd9
NIce <dsanchezc@vectoritcgroup.com>
2016-05-14 18:41:40 +0200

    a

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({secret:"Quiz 2016", resave: false, saveUninitialized:true});
+app.use(session({secret:"Quiz 2016", resave: false, saveUninitialized:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
 
